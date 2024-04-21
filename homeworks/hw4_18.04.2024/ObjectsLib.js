9637b2797188bd9a7d3f3f83734d20f75dfc30c0
przemyslawkrepski <krepski80@gmail.com>
2024-04-21 20:50:59 +0200

    after sending

--- homeworks/hw4_18.04.2024/ObjectsLib.js
+++ homeworks/hw4_18.04.2024/ObjectsLib.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * @fileoverview ObjectsLib - a library offering advanced object modifying functions.
  *
  * This library provides a set of functions for manipulating objects , including:
@@ -26,144 +26,144 @@
  * @see For more information and additional examples, refer to the README.md file.
  */
 
-class ObjectsLib {
-  //Task 2:  Object Property Enumeration and Deletion
-  /**
-   *
-   * @param {*} obj Object with field price and quantity
-   * @returns {number} Results of the calculation
-   */
-
-  static getTotalPrice(obj) {
-    return (
-      Object.getOwnPropertyDescriptor(obj, "price").value *
-      Object.getOwnPropertyDescriptor(obj, "quantity").value
-    );
-  }
-  /**
-   * Deletes the specified property from the object if it is configurable.
-   *
-   * @param {Object} obj The object from which to delete the property.
-   * @param {string} prop The name of the property to delete.
-   * @throws {Error} If the property is non-configurable.
-   */
-  static deleteNonConfigurable(obj, prop) {
-    if (Object.getOwnPropertyDescriptor(obj, prop)) {
-      if (Object.getOwnPropertyDescriptor(obj, prop).configurable === true) {
-        delete obj[prop];
+ class ObjectsLib {
+    //Task 2:  Object Property Enumeration and Deletion
+    /**
+     *
+     * @param {*} obj Object with field price and quantity
+     * @returns {number} Results of the calculation
+     */
+  
+    static getTotalPrice(obj) {
+      return (
+        Object.getOwnPropertyDescriptor(obj, "price").value *
+        Object.getOwnPropertyDescriptor(obj, "quantity").value
+      );
+    }
+    /**
+     * Deletes the specified property from the object if it is configurable.
+     *
+     * @param {Object} obj The object from which to delete the property.
+     * @param {string} prop The name of the property to delete.
+     * @throws {Error} If the property is non-configurable.
+     */
+    static deleteNonConfigurable(obj, prop) {
+      if (Object.getOwnPropertyDescriptor(obj, prop)) {
+        if (Object.getOwnPropertyDescriptor(obj, prop).configurable === true) {
+          delete obj[prop];
+        } else {
+          throw new Error(`Cannot delete non-configurable property: ${prop}`);
+        }
       } else {
-        throw new Error(`Cannot delete non-configurable property: ${prop}`);
+        console.log(`There is no property named ${prop} at this object`);
       }
-    } else {
-      console.log(`There is no property named ${prop} at this object`);
     }
-  }
-
-  //   Task 4: Advanced Property Descriptors
-
-  /**
-   * Creates an immutable copy of the provided object.
-   *
-   * @param {Object} obj The object to make immutable.
-   * @returns {Object} An immutable copy of the original object.
-   */
-  static createImmutableObject(obj) {
-    if (typeof obj !== "object" || obj === null) {
-      return obj;
-    }
-
-    return Object.keys(obj).reduce(
-      (clone, key) => {
-        const value = obj[key];
-        clone[key] =
-          typeof value === "object" && value !== null
-            ? this.createImmutableObject(value)
-            : value;
-
-        Object.defineProperty(clone, key, {
-          value: clone[key],
-          writable: false,
-          enumerable: true,
-          configurable: false,
-        });
-
-        return clone;
-      },
-      Array.isArray(obj) ? [] : {}
-    );
-  }
-
-  //   Task 5: Object Observation
-  /**
-   * Observes the provided object and invokes a callback whenever a property is accessed or modified.
-   *
-   * @param {Object} obj The object to observe.
-   * @param {Function} callback The callback function to invoke on property access or modification.
-   * @returns {Object} A proxy object for the original object with observation capabilities.
-   */
-  static observeObject(obj, callback) {
-    return new Proxy(obj, {
-      get(target, prop, receiver) {
-        callback(prop, "get");
-        return Reflect.get(target, prop, receiver);
-      },
-      set(target, prop, value, receiver) {
-        callback(prop, "set");
-        return Reflect.set(target, prop, value, receiver);
-      },
-    });
-  }
-  //   Task 6: Object Deep Cloning
-  /**
-   * Creates a deep clone of the provided object.
-   *
-   * @param {Object} obj The object to clone.
-   * @param {Map} [cloned=new Map()] A map to keep track of cloned objects and avoid circular references.
-   * @returns {Object} A deep clone of the original object.
-   */
-  static deepCloneObject(obj, cloned = new Map()) {
-    if (typeof obj !== "object" || obj === null) {
-      return obj;
-    }
-    if (cloned.has(obj)) {
-      return cloned.get(obj);
-    }
-    const clone = Array.isArray(obj) ? [] : {};
-
-    cloned.set(obj, clone);
-
-    return Object.keys(obj).reduce((acc, key) => {
-      acc[key] = this.deepCloneObject(obj[key], cloned);
-      return acc;
-    }, clone);
-  }
-  //   Task 7: Object Property Validation
-
-  /**
-   * Validates an object against a schema.
-   *
-   * @param {Object} obj The object to validate.
-   * @param {Object} schema The schema object to validate against.
-   * @returns {boolean} true if the object is valid according to the schema, otherwise false.
-   */
-  static validateObject(obj, schema) {
-    for (const key in schema) {
-      if (!obj.hasOwnProperty(key)) {
-        return false;
+  
+    //   Task 4: Advanced Property Descriptors
+  
+    /**
+     * Creates an immutable copy of the provided object.
+     *
+     * @param {Object} obj The object to make immutable.
+     * @returns {Object} An immutable copy of the original object.
+     */
+    static createImmutableObject(obj) {
+      if (typeof obj !== "object" || obj === null) {
+        return obj;
       }
-
-      if (typeof schema[key] === "object") {
-        if (
-          typeof obj[key] !== "object" ||
-          !this.validateObject(obj[key], schema[key])
-        ) {
+  
+      return Object.keys(obj).reduce(
+        (clone, key) => {
+          const value = obj[key];
+          clone[key] =
+            typeof value === "object" && value !== null
+              ? this.createImmutableObject(value)
+              : value;
+  
+          Object.defineProperty(clone, key, {
+            value: clone[key],
+            writable: false,
+            enumerable: true,
+            configurable: false,
+          });
+  
+          return clone;
+        },
+        Array.isArray(obj) ? [] : {}
+      );
+    }
+  
+    //   Task 5: Object Observation
+    /**
+     * Observes the provided object and invokes a callback whenever a property is accessed or modified.
+     *
+     * @param {Object} obj The object to observe.
+     * @param {Function} callback The callback function to invoke on property access or modification.
+     * @returns {Object} A proxy object for the original object with observation capabilities.
+     */
+    static observeObject(obj, callback) {
+      return new Proxy(obj, {
+        get(target, prop, receiver) {
+          callback(prop, "get");
+          return Reflect.get(target, prop, receiver);
+        },
+        set(target, prop, value, receiver) {
+          callback(prop, "set");
+          return Reflect.set(target, prop, value, receiver);
+        },
+      });
+    }
+    //   Task 6: Object Deep Cloning
+    /**
+     * Creates a deep clone of the provided object.
+     *
+     * @param {Object} obj The object to clone.
+     * @param {Map} [cloned=new Map()] A map to keep track of cloned objects and avoid circular references.
+     * @returns {Object} A deep clone of the original object.
+     */
+    static deepCloneObject(obj, cloned = new Map()) {
+      if (typeof obj !== "object" || obj === null) {
+        return obj;
+      }
+      if (cloned.has(obj)) {
+        return cloned.get(obj);
+      }
+      const clone = Array.isArray(obj) ? [] : {};
+  
+      cloned.set(obj, clone);
+  
+      return Object.keys(obj).reduce((acc, key) => {
+        acc[key] = this.deepCloneObject(obj[key], cloned);
+        return acc;
+      }, clone);
+    }
+    //   Task 7: Object Property Validation
+  
+    /**
+     * Validates an object against a schema.
+     * 
+     * @param {Object} obj The object to validate.
+     * @param {Object} schema The schema object to validate against.
+     * @returns {boolean} true if the object is valid according to the schema, otherwise false.
+     */
+    static validateObject(obj, schema) {
+      for (const key in schema) {
+        if (!obj.hasOwnProperty(key)) {
           return false;
         }
-      } else if (typeof obj[key] !== schema[key]) {
-        return false;
+  
+        if (typeof schema[key] === "object") {
+          if (
+            typeof obj[key] !== "object" ||
+            !this.validateObject(obj[key], schema[key])
+          ) {
+            return false;
+          }
+        } else if (typeof obj[key] !== schema[key]) {
+          return false;
+        }
       }
+      return true;
     }
-    return true;
   }
-}
-module.exports = ObjectsLib;
+  module.exports = ObjectsLib;
