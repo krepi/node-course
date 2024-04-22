3dccd537fc50a2177809398279577969c6b47baa
przemyslawkrepski <krepski80@gmail.com>
2024-04-22 21:10:53 +0200

    skeleton

---
+++ homeworks/hw5_22.04.2024/ArrayLibrary.js
@@ -0,0 +1,15 @@
+class ArrayLibrary {
+  // Task 1: Advanced Array Filtering
+  static customFilterUnique(array, callback) {}
+  // Task 2: Array Chunking
+  static chunkArray(array,chunk) {}
+  //Task 3: Array Shuffling
+  static customShuffle(array) {}
+  //Task 4: Array Intersection and Union
+  static getArrayIntersection(arrayOne, arrayTwo) {}
+  static getArrayUnion(arrayOne, arrayTwo) {}
+  //Task 5: Array Performance Analysis
+  static measureArrayPerformance(array, fn){}
+}
+
+exports.modules = ArrayLibrary;
