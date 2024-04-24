ae4ffc5fa89de28b9222084ffc14dcf41b649777
przemyslawkrepski <krepski80@gmail.com>
2024-04-24 10:22:01 +0200

    app.js changes

--- homeworks/hw5_22.04.2024/app.js
+++ homeworks/hw5_22.04.2024/app.js
@@ -1,5 +1,7 @@
 const ArrayLibrary = require("./ArrayLibrary.js");
 
+//Datas
+
 const objectsArray = [
   { id: 1, name: "Anna" },
   { id: 2, name: "Anna" },
@@ -12,18 +14,29 @@ const b = a;
 const arr1 = [1, 2, 3];
 const arr2 = [{ name: 1 }, { name: 2 }, { name: 2 }, b, a, 3, 2];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 //Task:1
 const uniqueByName = ArrayLibrary.customFilterUnique(
   objectsArray,
   (obj) => obj.name
 );
 
+//Task: 2
+const chunkies = ArrayLibrary.chunkArray(objectsArray, 3);
+
+// Task: 3
+
+const shuffledNumbers = ArrayLibrary.customShuffle(numbers);
+
+//Task: 4
 const union = ArrayLibrary.getArrayUnion(arr1, arr2);
 const intersection = ArrayLibrary.getArrayIntersection(arr1, arr2);
 
-const chunkies = ArrayLibrary.chunkArray(objectsArray, 3);
-
-const shuffledNumbers = ArrayLibrary.customShuffle(numbers);
+//Task: 5
+const timeCustomFilter = ArrayLibrary.measureArrayPerformance(
+  ArrayLibrary.customFilterUnique,
+  [objectsArray, (obj) => obj.name]
+);
 
 const timeChunkie = ArrayLibrary.measureArrayPerformance(
   ArrayLibrary.chunkArray,
@@ -34,6 +47,11 @@ const timeUnion = ArrayLibrary.measureArrayPerformance(
   [arr1, arr2]
 );
 
+const timeShuffle = ArrayLibrary.measureArrayPerformance(
+  ArrayLibrary.customShuffle,
+  [numbers]
+);
+
 console.log("Custom filtred ", uniqueByName);
 console.log("Union ", union);
 console.log("intersction ", intersection);
@@ -41,5 +59,7 @@ console.log("Chunkies ", chunkies);
 console.log("Original array:", numbers);
 console.log("Shuffled array:", shuffledNumbers);
 
-console.log("time =", timeChunkie);
-console.log("time Union = ", timeUnion);
+console.log("time Filter =", timeCustomFilter);
+console.log("time Chunkie =", timeChunkie);
+console.log("time Union =", timeUnion);
+console.log("time Shuffle =", timeShuffle);
