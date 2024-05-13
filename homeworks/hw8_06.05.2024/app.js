cc487084f48a39f8afc626d2b56dc40140201b13
przemyslawkrepski <krepski80@gmail.com>
2024-05-13 20:24:52 +0200

    some work with cart

--- homeworks/hw8_06.05.2024/app.js
+++ homeworks/hw8_06.05.2024/app.js
@@ -1,10 +1,9 @@
 import User from './users/User.js';
-
 import Cart from './services/Cart.js';
 import Order from './services/Order.js';
 
-import {Book, SciFiBook, AdventureBook} from './publications/Books.js';
-import {Magazine, SportsMagazine, FashionMagazine} from './publications/Magazines.js';
+import {SciFiBook, AdventureBook} from './publications/Books.js';
+import {SportsMagazine, FashionMagazine} from './publications/Magazines.js';
 
 
 const user = new User("John Doe", "john@doe.com", "1");
@@ -13,7 +12,7 @@ const user = new User("John Doe", "john@doe.com", "1");
 const sciFiBook = new SciFiBook("Dune", "Frank Herbert", 9.99, true, "Dune Universe");
 const adventureBook = new AdventureBook("The Hobbit", "J.R.R. Tolkien", 12.99, true, "Middle Earth");
 const sportsMagazine = new SportsMagazine("Sports Illustrated", "August 2021", 4.99, true, "Football");
-
+const fashionMagazine = new FashionMagazine("Vogue", "August 2021", 5.99, true, "Spring Collections");
 
 console.log(sciFiBook.getDescription())
 
@@ -21,10 +20,12 @@ const firstCart = new Cart(user);
 firstCart.addItem(sciFiBook);
 firstCart.addItem(adventureBook);
 firstCart.addItem(sportsMagazine);
+firstCart.addItem(fashionMagazine);
 console.log(firstCart.showItems());
-firstCart.clearCart()
+
+// firstCart.clearCart()
 
 
 const firstOrder = Order.createOrder(firstCart)
 
-// console.log(`Order Id: ${firstOrder.getId()}, Summary: ${firstOrder.totalPrice}`)
+console.log(`Order Id: ${firstOrder.getId()}, Summary: ${firstOrder.totalPrice}`)
